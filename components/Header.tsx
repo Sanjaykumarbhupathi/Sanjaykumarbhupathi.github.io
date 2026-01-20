df835d259e2192235e6d90475bc99e0c4e31c8dd
Sanjay Kumar Bhupathi <sanjay.bhupathi@quidcash.in>
2026-01-21 00:42:33 +0530

    dirty commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -52,6 +52,7 @@ export function Header() {
                     ))}
                 </div>
 
+
                 {/* Right side actions */}
                 <div className="flex items-center gap-3">
                     <DarkThemeToggle />
